cd48c036f85a1811302eb2910ac4a184fa2a1a7d
MS2022Singh <106476125+MS2022Singh@users.noreply.github.com>
2026-02-13 14:38:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import requests, io, os, zipfile
 from PIL import Image, ImageEnhance, ImageFilter
 from rembg import remove, new_session
-session = new_session("u2netp")   # lightweight model
+session = None   # lightweight model
 
 app = FastAPI(title="Amazon Image Auto Editor")
 
@@ -45,7 +45,12 @@ def internal_white_bg(img_bytes):
 def remove_bg_safe(image_bytes):
     try:
         # Local AI segmentation (FREE unlimited)
-        output = remove(image_bytes, session=session)
+        global session
+if session is None:
+    session = new_session("u2netp")
+
+output = remove(image_bytes, session=session)
+
 
         # Safety check — agar segmentation fail ho jaye
         if not output or len(output) < 1000:
@@ -172,6 +177,7 @@ async def removebg_test(file: UploadFile = File(...)):
     img_bytes = await file.read()
     out = remove_bg_safe(img_bytes)
     return StreamingResponse(io.BytesIO(out), media_type="image/png")
+
 
 
 
